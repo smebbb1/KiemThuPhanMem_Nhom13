07bad1d65c55a768c32666d140b517ee1cb04e6d
DoNguyenThanhNguyen <125115637+smebbb1@users.noreply.github.com>
2025-06-07 11:10:56 +0700

    Update Constant.java

--- src/test/java/Common/Constant/Constant.java
+++ src/test/java/Common/Constant/Constant.java
@@ -6,6 +6,6 @@ public class Constant {
     public static  final String RAILWAY_URL ="http://railwayb1.somee.com/Page/HomePage.cshtml";
     public static  final String RAILWAY_URL1 ="http://railwayb1.somee.com/Page/BookTicketPage.cshtml";
     public static final String USERNAME = "nguyen@gmail.com";
-    public static final String PASSWORD ="1234512345";
+    public static final String PASSWORD ="";
 
 }

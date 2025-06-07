259c5282acd64a5ccbf59906a9bdace00a5a3d05
DoNguyenThanhNguyen <125115637+smebbb1@users.noreply.github.com>
2025-06-07 11:22:30 +0700

    Update LoginTest.java

--- src/test/java/Testcases/Railway/LoginTest.java
+++ src/test/java/Testcases/Railway/LoginTest.java
@@ -12,37 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-public class LoginTest {
-    @BeforeMethod
-    public void beforeMethod() {
-        System.out.println("Pre-condition");
-        Constant.WEBDRIVER = new ChromeDriver();
-        Constant.WEBDRIVER.manage().window().maximize();
-    }
-
-    @AfterMethod
-    public void afterMethod() {
-        System.out.println("Post-condition");
-
-        Constant.WEBDRIVER.quit();
-    }
-
-    @Test
-    public void TC01() {
-        System.out.println("TC01 - User can log into Raiway with valid username and password");
-        HomePage homePage = new HomePage();
-        homePage.open();
-
-        LoginPage loginPage = homePage.gotoLoginPage();
-
-        Str actualMsg = loginPage.login(Constant.USERNAME, Constant.PASSWORD).getLblWelcomeMessage();
+import org.testng.annotationsString actualMsg = loginPage.login(Constant.USERNAME, Constant.PASSWORD).getLblWelcomeMessage();
         String expectedMsg = "Welcome " + Constant.USERNAME;
 
         Assert.assertEquals(actualMsg, expectedMsg, "Welcome message is not displayed as expected");

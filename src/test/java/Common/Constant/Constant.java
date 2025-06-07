0ef3864047c3187d8dfecd5038566c27414b90d3
DoNguyenThanhNguyen <125115637+smebbb1@users.noreply.github.com>
2025-06-07 11:08:36 +0700

    Update Constant.java

--- src/test/java/Common/Constant/Constant.java
+++ src/test/java/Common/Constant/Constant.java
@@ -5,7 +5,7 @@ public class Constant {
     public static WebDriver WEBDRIVER;
     public static  final String RAILWAY_URL ="http://railwayb1.somee.com/Page/HomePage.cshtml";
     public static  final String RAILWAY_URL1 ="http://railwayb1.somee.com/Page/BookTicketPage.cshtml";
-    public static final String USERNAME = "dangtest14a@gmail.com";
+    public static final String USERNAME = "nguyen@gmail.com";
     public static final String PASSWORD ="1234512345";
 
 }
